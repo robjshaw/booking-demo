78b6307aa4a94edf2406d2f87b7b8e4c94da240b
Rob Shaw <rshaw@twilio.com>
2021-03-31 09:09:35 +1100

    initial commit

---
+++ functions/inbound_check.js
@@ -0,0 +1,32 @@
+exports.handler = function(context, event, callback) {
+
+    var Airtable = require('airtable');
+    var base = new Airtable({apiKey: process.env.AIRTABLEKEY}).base(process.env.AIRTABLEBASE);
+    
+    var response = {};
+
+    response.known = false;
+    response.name = "";
+    response.product = "";
+    response.product_type = "";
+
+    console.log('+'+ event.from);
+
+    base('custRecords').select({
+        filterByFormula: `{phoneNumber} = "${event.from}"`
+    }).eachPage(function page(records, fetchNextPage) {
+        
+        records.forEach(function(record) {
+
+            response.known = true;
+            response.name = record.get('firstName');
+            response.product = record.get('productModelA');
+            response.product_type = record.get('productTypeA');
+
+        });
+
+        callback(null, response);
+
+    });
+
+}
